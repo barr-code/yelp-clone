175287da0e3f3c49bcf7e966eba802092a73cf76
Victoria Barr <vb329@georgetown.edu>
2014-11-03 16:02:39 +0000

    Users can delete restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -66,4 +66,18 @@ describe 'restaurants' do
 		end
 	end
 
+	context 'deleting a restaurant' do
+		before do
+			Restaurant.create(:name => 'KFC')
+		end
+
+		it "removes a restaurant when a user clicks a delete link" do
+			visit '/restaurants'
+			click_link 'Delete KFC'
+			expect(page).not_to have_content 'KFC'
+			expect(page).to have_content 'Restaurant deleted successfully'
+		end
+
+	end
+
 end
