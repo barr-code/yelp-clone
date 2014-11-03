25ac5fd8857644f69d572aee04b52f32359d3fbb
Victoria Barr <vb329@georgetown.edu>
2014-11-03 14:57:54 +0000

    Added rating property to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141103120325) do
+ActiveRecord::Schema.define(version: 20141103145537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141103120325) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rating"
   end
 
 end
