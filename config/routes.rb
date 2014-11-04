0721d53ce027d807b040c4baa579bbef2b48b43c
Victoria Barr <vb329@georgetown.edu>
2014-11-04 14:07:30 +0000

    Devise gem incorporated; users can sign in, sign up, and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :restaurants do
     resources :reviews
   end
+
+  root to: "restaurants#index"
+
 end
 
   # get 'restaurants'
